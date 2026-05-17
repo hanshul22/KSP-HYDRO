df8da79cea80562c0b2322d049f20965daf1de3f
hanshul22 <hanshulkumawat22@gmail.com>
2026-05-17 10:37:47 +0530

    deployment error fixed

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as ContactCTA } from './common/ContactCTA.jsx';
-export { default as OptimizedImage } from './common/OptimizedImage.jsx';
+export { default as OptimizedImage } from './common/Optimizedimage.jsx';
 export { default as OptimizedVideo } from './common/Optimizedvideo.jsx';
